d45b2ee848ec674ff4d163ecfadf592849c3c242
Ilja Tkachuk <timcf@Iljas-iMac.local>
2018-02-01 16:29:44 +0200

    rm changex

--- mix.exs
+++ mix.exs
@@ -23,7 +23,6 @@ defmodule Boilex.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:changex, "~> 0.1.1", only: [:dev, :test], runtime: false},
     ]
   end
 end
